3190486d86902536e5c9e30689957985afe8c518
charleshpalmer <73086397+charleshpalmer@users.noreply.github.com>
2021-05-31 14:07:02 +0100

    Adding Add Contact Route Using JWT (Allows For Individual Contact Lists By User ID)

--- routes/contacts.js
+++ routes/contacts.js
@@ -31,9 +31,45 @@ router.get('/', auth, async (req, res) => {
 // @route       POST api/contacts
 // @desc        Add New Contacts
 // @access      Private
-router.post('/', (req, res) => {
+router.post('/', [auth, [
 
-    res.send('Add Contact');
+    check('name', 'Name is required.').not().isEmpty()
+
+]], async (req, res) => {
+
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()) {
+
+        return res.status(400).json({ errors: errors.array() });
+
+    }
+
+    const { name, email, phone, type } = req.body;
+
+    try {
+        
+        const newContact = new Contact({
+
+            name,
+            email,
+            phone,
+            type,
+            user: req.user.id
+
+        });
+
+        const contact = await newContact.save();
+
+        res.json(contact);
+
+    } catch (err) {
+     
+        console.error(err.message);
+
+        res.status(500).send('Server Error');
+
+    }
 
 });
 
