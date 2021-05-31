b043b74fbc74cd8925dcc7ed0bb314d32527711e
charleshpalmer <73086397+charleshpalmer@users.noreply.github.com>
2021-05-31 12:06:52 +0100

    Setting Up JSON Web Token For User Registration

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const router = express.Router();
 const { check, validationResult } = require('express-validator/check'); 
 const User = require('../models/User');
@@ -50,7 +52,27 @@ router.post('/', [
 
         await user.save();
 
-        res.send('User Saved.');
+        const payload = {
+
+            user: {
+
+                id: user.id
+
+            }
+
+        }
+
+        jwt.sign(payload, config.get('jwtSecret'), {
+
+            expiresIn: 360000
+
+        }, (err, token) => {
+
+            if(err) throw err;
+
+            res.json({ token });
+
+        });
 
     } catch (err) {
         
