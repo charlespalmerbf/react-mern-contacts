6e171f903ba08adc6dd184cb1060c292f70dc6f9
charleshpalmer <73086397+charleshpalmer@users.noreply.github.com>
2021-05-31 13:08:54 +0100

    Adding Protected Routes Using Auth Middleware and JWT

--- routes/auth.js
+++ routes/auth.js
@@ -3,15 +3,28 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config');
+const auth = require('../middleware/auth');
 const { check, validationResult } = require('express-validator/check'); 
 const User = require('../models/User');
 
 // @route       GET api/auth
 // @desc        Get Logged In User
 // @access      Private
-router.get('/', (req, res) => {
+router.get('/', auth, async (req, res) => {
 
-    res.send('Get Logged In User');
+    try {
+        
+        const user = await User.findById(req.user.id).select('-password');
+
+        res.json(user);
+
+    } catch (err) {
+        
+        console.error(err.message);
+
+        res.status(500).send('Server Error');
+
+    }
 
 });
 
