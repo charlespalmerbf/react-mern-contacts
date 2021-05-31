a68b04c8985dcd82087e97d0381ad3e1e362caf8
charleshpalmer <73086397+charleshpalmer@users.noreply.github.com>
2021-05-31 10:48:56 +0100

    Add User To Database & Hash Password

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
 const router = express.Router();
 const { check, validationResult } = require('express-validator/check'); 
 const User = require('../models/User');
@@ -13,7 +14,7 @@ router.post('/', [
     check('email', 'Please include a valid email.').isEmail(),
     check('password', 'Please enter a password with six or more characters.').isLength({ min: 6 })
 
-], (req, res) => {
+], async (req, res) => {
 
     const errors = validationResult(req);
 
@@ -23,7 +24,41 @@ router.post('/', [
 
     }
 
-    res.send('Passed Validation');
+    const { name, email, password } = req.body;
+
+    try {
+        
+        let user = await User.findOne({ email });
+
+        if(user) {
+
+            return res.status(400).json({ msg: 'User Already Exists' })
+
+        }
+
+        user = new User({
+
+            name,
+            email,
+            password
+
+        });
+
+        const salt = await bcrypt.genSalt(10);
+
+        user.password = await bcrypt.hash(password, salt);
+
+        await user.save();
+
+        res.send('User Saved.');
+
+    } catch (err) {
+        
+        console.error(err.message);
+
+        res.status(500).send('Server Error');
+
+    }
 
 });
 
