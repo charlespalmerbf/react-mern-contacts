79a4de26e46f2cb80fe0947e1c2fea84b7c6714c
charleshpalmer <73086397+charleshpalmer@users.noreply.github.com>
2021-06-02 18:51:05 +0100

    REACT Creating Register & Login Component UI

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 import Navbar from './components/layout/Navbar';
 import ContactState from './context/contact/ContactState';
 import AuthState from './context/auth/AuthState';
@@ -28,6 +30,10 @@ const App = () => {
                 <Route exact path = '/' component={Home} />
 
                 <Route exact path = '/about' component={About} />
+
+                <Route exact path = '/register' component={Register} />
+
+                <Route exact path = '/login' component={Login} />
 
             </Switch> 
 
